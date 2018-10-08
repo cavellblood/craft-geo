95d4e24735128bd5a5f1c4357649323036e39260
Cavell Blood <cavellblood@me.com>
2018-10-08 17:26:29 -0400

    default IP is set to 8.8.8.8
    Signed-off-by: Cavell Blood <cavellblood@me.com>

--- geo/config.php
+++ geo/config.php
@@ -1,9 +1,11 @@
 <?php
 
 return array(
-  // This is the IP address used if in devMode or visiting your site
-  // from the local computer to the server.
-  "defaultIp"=>"190.93.246.7", // Costa Rica
-  // "defaultIp"=>"92.111.6.18", Netherland no city or region
-  "cacheTime"=>"43200" //seconds
+    // This is the IP address used if in devMode or visiting your site
+    // from the local computer to the server.
+    // i.e. 8.8.8.8 - Google
+    'defaultIp' => '8.8.8.8',
+
+    // Number of seconds to cache the results
+    'cacheTime' => '43200',
 );
