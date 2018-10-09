d0ed70fa21c0dcfa0a9ff3419cbd6aa71f1a7213
Cavell Blood <cavellblood@me.com>
2018-10-08 20:33:42 -0400

    added plugin url functions
    Signed-off-by: Cavell Blood <cavellblood@me.com>

--- geo/GeoPlugin.php
+++ geo/GeoPlugin.php
@@ -23,6 +23,27 @@ class GeoPlugin extends BasePlugin
         return 'http://lukeholder.com';
     }
 
+    public function getPluginUrl()
+    {
+        return 'https://github.com/lukeholder/craft-geo';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDocumentationUrl()
+    {
+        return $this->getPluginUrl().'/blob/master/readme.md';
+    }
+
+    /**
+     * @return string
+     */
+    public function getReleaseFeedUrl()
+    {
+        return 'https://raw.githubusercontent.com/lukeholder/craft-geo/master/releases.json';
+    }
+
     public function init() {
         require_once 'vendor/autoload.php';
     }
