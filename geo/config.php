772c1278677f70de0041025899b539218c8584f3
Cavell Blood <cavellblood@me.com>
2018-10-08 21:30:23 -0400

    update code comments
    Signed-off-by: Cavell Blood <cavellblood@me.com>

--- geo/config.php
+++ geo/config.php
@@ -1,16 +1,23 @@
 <?php
 
 return array(
-    // This is the IP address used if in devMode or visiting your site
-    // from the local computer to the server.
-    // i.e. 8.8.8.8 - Google
+    /**
+     * This is the IP address used if in devMode or visiting your site
+     * from the local computer to the server.
     'defaultIp' => '8.8.8.8',
-
-    // Number of seconds to cache the results
+     * i.e. 8.8.8.8 - Google
+     */
     'cacheTime' => '43200',
 
-    // API Key for IPAPI.com - which replaced Nekudo on 2018/10/08
-    // https://github.com/nekudocom/shiny_geoip/blob/master/README.md#readme
-    // A free account can be created here: https://ipapi.com/product
+    /**
+     * Number of seconds to cache the results
+     */
     'ipApiKey' => 'APIKEYHERE'
+
+
+    /**
+     * API Key for IPAPI.com - which replaced Nekudo on 2018/10/08
+     * https://github.com/nekudocom/shiny_geoip/blob/master/README.md#readme
+     * A free account can be created here: https://ipapi.com/product
+     */
 );
