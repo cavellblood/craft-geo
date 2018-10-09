73129e705faccc52ce33e122a64a948e21b66555
Cavell Blood <cavellblood@me.com>
2018-10-08 21:30:54 -0400

    comment out config settings
    Signed-off-by: Cavell Blood <cavellblood@me.com>

--- geo/config.php
+++ geo/config.php
@@ -4,20 +4,19 @@ return array(
     /**
      * This is the IP address used if in devMode or visiting your site
      * from the local computer to the server.
-    'defaultIp' => '8.8.8.8',
      * i.e. 8.8.8.8 - Google
      */
-    'cacheTime' => '43200',
+    // 'defaultIp' => '8.8.8.8',
 
     /**
      * Number of seconds to cache the results
      */
-    'ipApiKey' => 'APIKEYHERE'
-
-
+    // 'cacheTime' => '43200',
+    
     /**
      * API Key for IPAPI.com - which replaced Nekudo on 2018/10/08
      * https://github.com/nekudocom/shiny_geoip/blob/master/README.md#readme
      * A free account can be created here: https://ipapi.com/product
      */
+    // 'ipApiKey' => 'APIKEYHERE'
 );
